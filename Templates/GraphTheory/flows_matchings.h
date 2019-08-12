ba634b7c9b428d26bb0522956d33554ab92adae7
forgottencsc <forgottencosecant@outlook.com>
2019-08-13 01:45:31 +0800

    -

--- Templates/GraphTheory/flows_matchings.h
+++ Templates/GraphTheory/flows_matchings.h
@@ -2,6 +2,8 @@
 #define N 10001
 using namespace std;
 
+
+//  ISAP
 struct edge { int p, v; ll c, f; };
 vector<edge> g[N];
 int cur[N], dis[N], gap[N], nc;
@@ -117,3 +119,52 @@ struct PrimalDual {
         return { f0, ret };
     }
 };
+
+//  Hopcroft-Karp Algorithm
+vector<int> g[N]; int n1, n2;
+int lnk[N], dis[N], dm; bool vis[N];
+void init(int n, int m) {
+    n1 = n; n2 = m;
+    for (int i = 1; i <= n1 + n2; ++i)
+        g[i].clear();
+}
+
+bool bfs_hk() {
+    queue<int> q; dm = INT_MAX;
+    fill(dis + 1, dis + n1 + n2 + 1, -1);
+    for (int i = 1; i <= n1; ++i)
+        if (!lnk[i]) q.push(i), dis[i] = 0;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        if (dis[u] > dm) break;
+        for (int v : g[u]) {
+            if (dis[v] != -1) continue;
+            dis[v] = dis[u] + 1;
+            if (!lnk[v]) dm = dis[v];
+            else dis[lnk[v]] = dis[v] + 1, q.push(lnk[v]);
+        }
+    }
+    return dm != INT_MAX;
+}
+
+int dfs_hk(int u) {
+    for (int v : g[u]) {
+        if (vis[v] || dis[v] != dis[u] + 1) continue;
+        vis[v] = 1;
+        if (lnk[v] && dis[v] == dm) continue;
+        if (lnk[v] && !dfs_hk(lnk[v])) continue;
+        lnk[v] = u; lnk[u] = v; return 1;
+    }
+    return 0;
+}
+
+int hk() {
+    fill (lnk + 1, lnk + n1 + n2 + 1, 0);
+    int res = 0;
+    while (bfs_hk()) {
+        fill (vis + 1, vis + n1 + n2 + 1, 0);
+        for (int i = 1; i <= n1; ++i)
+            if (!lnk[i] && dfs_hk(i)) res++;
+    }
+    return res;
+}
