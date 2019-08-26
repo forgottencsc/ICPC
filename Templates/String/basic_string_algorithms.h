391dc697b4b6db269326ef126b8c0d95b57cca6b
forgottencsc <forgottencosecant@outlook.com>
2019-08-27 00:20:13 +0800

    Update basic_string_algorithms.h

--- Templates/String/basic_string_algorithms.h
+++ Templates/String/basic_string_algorithms.h
@@ -64,3 +64,63 @@ void getr(const string& s, vector<int>& r) {
 bool ispal(vector<int>& r, int l, int u) {
     return r[l + u] >= u - l;
 }
+
+
+//  Hashing
+typedef long long ll;
+typedef pair<ll, ll> pll;
+const ll p1 = 1145140019, b1 = 893;
+const ll p2 = 1919810021, b2 = 364;
+ll hs1[N], hb1[N];
+ll hs2[N], hb2[N];
+
+char s[N]; int n;
+void genh() {
+    hb1[0] = hb2[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        hs1[i] = (hs1[i - 1] * b1 + s[i]) % p1;
+        hb1[i] = hb1[i - 1] * b1 % p1;
+        hs2[i] = (hs2[i - 1] * b2 + s[i]) % p2;
+        hb2[i] = hb2[i - 1] * b2 % p2;
+    }
+}
+
+pll geth(int l, int r) {
+    ll v1 = (hs1[r] - hs1[l - 1] * hb1[r - l + 1]) % p1;
+    ll v2 = (hs2[r] - hs2[l - 1] * hb2[r - l + 1]) % p2;
+    if (v1 < 0) v1 += p1; if (v2 < 0) v2 += p2;
+    return { v1, v2 };
+}
+
+hs concat(hs lh, int ln, hs rh, int rn) {
+    hs r; r.n = ln + rn;
+    r.v1 = (lh.v1 * hb1[rn] + rh.v1) % p1;
+    r.v2 = (lh.v2 * hb2[rn] + rh.v2) % p2;
+    return r;
+}
+
+int lcp(int u, int v) {
+    if (s[u] != s[v]) return 0;
+    int l = 1, r = n - max(u, v) + 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (geth(u, u + mid - 1) == geth(v, v + mid - 1))
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return r;
+}
+
+int lcs(int u, int v) {
+    if (s[u] != s[v]) return 0;
+    int l = 1, r = min(u, v);
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (geth(u - mid + 1, u) == geth(v - mid + 1, v))
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return r;
+}
