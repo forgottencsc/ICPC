ecee65bd72f3eec63d84a816a824aecd9c0dbac4
forgottencsc <forgottencosecant@outlook.com>
2019-09-19 16:14:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,106 +1,89 @@
-#include <ctime>
-#include <cstdio>
-#include <algorithm>
-#include <ctime>
-#include <iostream>
-#include <queue>
-#include <bitset>
-#include <string>
-#include <cstring>
-#include <set>
-#include <cmath>
-#define double long
+#include <bits/stdc++.h>
+#define N 600001
+#define M 2001
 using namespace std;
-struct point{
-    long double x, y;
-    point(long double x = 0, long double y= 0):x(x),y(y){}
-};
-point operator-(point a, point b) {
-    return point(a.x - b.x, a.y - b.y);
-}
-double operator*(point a, point b) {
-    return a.x * b.y - a.y * b.x;
-}
-double cross(point a, point b, point c) {
-    return (b - a) * (c - a);
-}
-bool operator==(point a, point b) {
-    return a.x == b.x && a.y == b.y;
-}
-int T, n, m;
-point a[10000], b;
-int l[10000], r[10000];
-int id[10000];
-vector<int> d, ans;
-int main() {
-    //freopen("1.in", "r", stdin);
-    scanf("%d", &T);
-    while (T --) {
-        scanf("%d %d", &n, &m);
-        for (int i = 1; i <= n ; i ++)
-            scanf("%lld%lld", &a[i].x, &a[i].y);
-        for (int i = 1; i <= m ; i ++)
-            id[i] = i, id[i+m]=i+m, id[i+m + m]=i+m + m;
-        for (int id= 1; id <= m ; id ++){
-            scanf("%lld%lld", &b.x, &b.y);
-            point c[2];
-            int tot = 0;
-            for (int i = 1; i <= n ; i ++)
-                if (cross(b, a[i], a[(i - 2 + n) % n + 1]) *cross(b, a[i], a[i % n + 1]) > 0 && cross(b, a[i], a[i % n + 1]) < 0)
-                    c[tot++] = a[i];
 
-            for (int i = 1; i <= n ; i ++)
-                if (cross(b, a[i], a[(i - 2 + n) % n + 1]) *cross(b, a[i], a[i % n + 1]) > 0 && cross(b, a[i], a[i % n + 1]) > 0)
-                    c[tot++] = a[i];
-            for (int i = 1; i <=n ; i ++) {
-                if (a[i] == c[0]) {
-                    l[id] = i;
-                }
-                if (a[i] == c[1])
-                    r[id] = i;
-            }
-            if (l[id] > r[id])
-                r[id] += n;
-            l[id + m] = l[id] + n;
-            r[id + m] = r[id] + n;
-            l[id + m+m] = l[id] + n+n;
-            r[id + m+m] = r[id] + n+n;
-        }
-        sort(id +1, id + m + m + 1,[](int x, int y){return l[x] < l[y];});
-        ans.resize(0);
-        for (int i = 1; i <= m + m; i ++)
-        {
-            d.resize(0);
-            d.push_back(id[i]);
-            int cnt = i + 1, mx = -1;
-            while (r[d[d.size() - 1]] - l[d[0]] + 1 < n + 1)
-            {
-                while (cnt <= m + m && l[id[cnt]] <= r[d[d.size() - 1]]) {
-                    if (mx == -1 ||r[id[cnt]] > r[mx])
-                        mx = id[cnt];
-                    cnt ++;
-                }
-                if (mx == -1 || r[mx] <= r[d[d.size() - 1]])
-                    break;
-                d.push_back(mx);
-            }
-            if (r[d[d.size() - 1]] - l[d[0]] + 1 >= n + 1 && (d.size() < ans.size() || !ans.size()) )
-                ans = d;
-        }
-        if (!ans.size())
-            printf("-1\n");
-        else {
-            printf("%d\n", ans.size());
-            for (auto u:ans)
-                if (u > m + m )
-                    printf("%d ", u - m - m);
-                else
-                if (u > m)
-                    printf("%d ", u - m);
-                else
-                    printf("%d ", u);
-            printf("\n");
-        }
+struct query_t { int t, u, v, k; }; qs[M];
+
+vector<int> g0[N];
+int mp[N][20], dep[N], dfn[N], dfc;
+
+void dfs_lca(int u, int f) {
+    dep[u] = dep[f] + 1; mp[u][0] = f; dfn[u] = ++dfc;
+    for (int i = 1; mp[u][i - 1]; ++i)
+        mp[u][i] = mp[mp[u][i - 1]][i - 1];
+    for (int v : g0[u]) if (v != f) dfs_lca(v, u);
+}
+
+int lca(int u, int v) {
+    if (dep[u] < dep[v]) swap(u, v);
+    for (int i = 19; dep[u] - dep[v]; --i)
+        if ((dep[u] - dep[v]) & (1 << i))
+            u = mp[u][i];
+    if (u == v) return u;
+    for (int i = 19; i >= 0; --i)
+        if (mp[u][i]) u = mp[u][i], v = mp[v][i];
+    return mp[u][0];
+}
+
+
+int s[N], t, r[N], m;
+int sz[N], val[N], fa[N];
+
+void conn(int u, int v) {
+    if (dep[v] - dep[u] != 1) {
+        int w = ++n;
+        fa[v] = w;
+        fa[w] = u;
+        r[++m] = w;
+        sz[w] = dep[v] - dep[u] - 1;
     }
+    else {
+        fa[v] = u;
+
+    }
+
+}
+
+void build_vt(vector<int>& us) {
+    sort(us.begin(), us.end(), [](int u, int v) { return dfn[u] < dfn[v]; });
+    for (int u : us) {
+        if (!t) { s[++t] = u; continue; }
+        int v = lca(s[t], u);
+        while (
+    }
+}
+
+void clear_vt() {
+    do g[r[m]].clear(); while (--m);
+}
+
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+    ifstream cin("1.in");
+    #endif // ONLINE_JUDGE
+
+    int T; cin >> T;
+    while(T--) {
+        int n, q; cin >> n >> q;
+        for (int i = 1; i <= n - 1; ++i) {
+            int u, v; cin >> u >> v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+
+        dfs_lca(1, 0);
+
+        for (int i = 1; i <= q; ++i) {
+            cin >> qs[i].t >> qs[i].u >> qs[i].v;
+            if (t <= 3 || t == 7) cin >> qs[i].k;
+        }
+
+
+
+    }
+
     return 0;
 }
