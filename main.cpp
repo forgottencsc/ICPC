1e2b4108d2e4f237cd826d33356653f2eef888c9
forgottencsc <forgottencosecant@outlook.com>
2019-09-20 21:50:06 +0800

    -

--- main.cpp
+++ main.cpp
@@ -1,49 +1,104 @@
 #include <bits/stdc++.h>
-#define N 500001
+#define N 100001
 using namespace std;
 
-int clr[N];
-vector<int> g[N];
-
-bool dfs(int u, int f) {
-    clr[u] = 3 - clr[f];
-    bool flg = 1;
-    for (int v : g[u]) {
-        if (!clr[v]) {
-            if (!dfs(v, u))
-                flg = 0;
-        }
-        else {
-            if (clr[u] + clr[v] != 3)
-                flg = 0;
-        }
-    }
-    return flg;
-}
+typedef long long ll;
+typedef vector<ll> vec;
+typedef vector<vec> mat;
 
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0);
-//    #ifndef ONLINE_JUDGE
-//    ifstream cin("1.in");
-//    #endif // ONLINE_JUDGE
-    int n, m; cin >> n >> m;
-    for (int i = 1; i <= m; ++i) {
-        int u, v; cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+    #ifndef ONLINE_JUDGE
+    ifstream cin("1.in");
+    #endif // ONLINE_JUDGE
+
+    int n1, n2; cin >> n1 >> n2;
+    mat a(n1, vec(n2));
+    for (int i = 0; i != n1; ++i)
+        for (int j = 0; j != n2; ++j)
+            cin >> a[i][j];
+
+    if (n1 > n2) {
+        mat r(n2, vec(n1));
+        for (int i = 0; i != n1; ++i)
+            for (int j = 0; j != n2; ++j)
+                r[j][i] = a[i][j];
+        a = move(r);
+        swap(n1, n2);
     }
 
-    clr[0] = 2;
-    int cs = 0, cc = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (!clr[i]) {
-            if (dfs(i, 0)) cc++;
-            cs++;
+    vec rs(n1, 0), cs(n2, 0);
+    for (int i = 0; i != n1; ++i)
+        for (int j = 0; j != n2; ++j)
+            rs[i] += a[i][j], cs[j] += a[i][j];
+
+    ll ans = 0, res = 0;
+
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for (int i = 0; i != n1; ++i) {
+        pq.push(rs[i]);
+        if (pq.size() > 4) pq.pop();
+    }
+
+    res = 0;
+    while (!pq.empty()) res += pq.top(), pq.pop();
+    ans = max(ans, res);
+
+    for (int j = 0; j != n2; ++j) {
+        for (int i = 0; i != n1; ++i)
+            rs[i] -= a[i][j];
+        for (int i = 0; i != n1; ++i) {
+            pq.push(rs[i]);
+            if (pq.size() > 3) pq.pop();
+        }
+        res = cs[j];
+        while (!pq.empty()) res += pq.top(), pq.pop();
+        ans = max(ans, res);
+        for (int i = 0; i != n1; ++i)
+            rs[i] += a[i][j];
+    }
+
+    for (int i1 = 0; i1 != n1 - 1; ++i1) {
+        for (int i2 = i1 + 1; i2 != n1; ++i2) {
+            for (int j = 0; j != n2; ++j)
+                cs[j] -= (a[i1][j] + a[i2][j]);
+            for (int j = 0; j != n2; ++j) {
+                pq.push(cs[j]);
+                if (pq.size() > 2) pq.pop();
+            }
+            res = rs[i1] + rs[i2];
+            while (!pq.empty())
+                res += pq.top(), pq.pop();
+            ans = max(ans, res);
+            for (int j = 0; j != n2; ++j)
+                cs[j] += (a[i1][j] + a[i2][j]);
         }
     }
 
-    cout << cs - 1 + (cc == cs) << endl;
+    for (int i = 0; i != n1; ++i) {
+        for (int j = 0; j != n2; ++j)
+            cs[j] -= a[i][j];
+        for (int j = 0; j != n2; ++j) {
+            pq.push(cs[j]);
+            if (pq.size() > 3) pq.pop();
+        }
+        res = rs[i];
+        while (!pq.empty()) res += pq.top(), pq.pop();
+        ans = max(ans, res);
+        for (int j = 0; j != n2; ++j)
+            cs[j] += a[i][j];
+    }
 
+    for (int i = 0; i != n2; ++i) {
+        pq.push(cs[i]);
+        if (pq.size() > 4) pq.pop();
+    }
+
+    res = 0;
+    while (!pq.empty()) res += pq.top(), pq.pop();
+    ans = max(ans, res);
+
+    cout << ans << endl;
 
     return 0;
 }
