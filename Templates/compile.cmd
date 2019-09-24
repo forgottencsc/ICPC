474d3f93ad5d9901d110458f97cef666eac33ea4
forgottencsc <forgottencosecant@outlook.com>
2019-09-25 00:28:58 +0800

    -

---
+++ Templates/compile.cmd
@@ -0,0 +1,6 @@
+pandoc -o 数学-组合.pdf Math/组合.md --pdf-engine=xelatex -VCJKmainfont="SimSun"
+pandoc -o 数学-数论.pdf Math/数论.md --pdf-engine=xelatex -VCJKmainfont="SimSun"
+pandoc -o 数学-几何.pdf Math/几何.md --pdf-engine=xelatex -VCJKmainfont="SimSun"
+pandoc -o 数学-杂项.pdf Math/杂项.md --pdf-engine=xelatex -VCJKmainfont="SimSun"
+pandoc -o 图论-杂项.pdf GraphTheory/杂项.md --pdf-engine=xelatex -VCJKmainfont="SimSun"
+pause
