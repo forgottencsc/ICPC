0dc916e3957e79f76dbd1f9907d1d7116a591635
forgottencsc <forgottencosecant@outlook.com>
2019-10-10 21:59:09 +0800

    -

--- Templates/compile.cmd
+++ Templates/compile.cmd
@@ -1,6 +1,7 @@
 pandoc -o 数据结构-基础.pdf DataStructures/基础.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
 pandoc -o 数据结构-树.pdf DataStructures/树据结构.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
 pandoc -o 图论-基础.pdf GraphTheory/杂项.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
+pandoc -o 图论-匹配与流.pdf GraphTheory/匹配与流.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
 pandoc -o 数学-杂项.pdf Math/杂项.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
 pandoc -o 数学-多项式.pdf Math/多项式.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
 pandoc -o 数学-组合.pdf Math/组合.md --pdf-engine=xelatex -VCJKoptions=BoldFont="SimHei" -VCJKmainfont="SimSun"
